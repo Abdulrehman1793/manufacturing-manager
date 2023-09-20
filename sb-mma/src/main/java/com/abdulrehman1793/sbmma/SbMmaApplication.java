d72011aab857e660096b3fc04f7b50497b02f8e5
Abdulrehman1793 <abdulrehman1793@gmail.com>
2023-09-20 22:01:15 +0530

    Add mma api using spring boot

---
+++ sb-mma/src/main/java/com/abdulrehman1793/sbmma/SbMmaApplication.java
@@ -0,0 +1,13 @@
+package com.abdulrehman1793.sbmma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMmaApplication.class, args);
+	}
+
+}
---
+++ sb-mma/src/test/java/com/abdulrehman1793/sbmma/SbMmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulrehman1793.sbmma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
