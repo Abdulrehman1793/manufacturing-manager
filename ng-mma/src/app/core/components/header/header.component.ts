ab6b0b92d8d40e92c1eaffe0da2abbcb3311bd4d
Abdulrehman1793 <abdulrehman1793@gmail.com>
2023-09-23 16:15:32 +0530

    Implement dynamic navigation on header

--- ng-mma/src/app/core/components/header/header.component.ts
+++ ng-mma/src/app/core/components/header/header.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  menus: Menu[] = [];
+  selectedMenu1: Menu[] = [];
 
+  ngOnInit(): void {
+    this.menus = [
+      { title: 'Dashboard', route: 'dashboard' },
+      {
+        title: 'Master`s',
+        children: [
+          { title: 'Staff & Resources', route: 'staff' },
+          { title: 'Customer', route: 'customer' },
+        ],
+      },
+      {
+        title: 'Settings',
+        children: [{ title: 'Unit Of Measures', route: 'uom' }],
+      },
+    ];
+  }
+
+  openMenu(menu: Menu) {
+    this.selectedMenu1 = menu.children || [];
+  }
+}
+
+interface Menu {
+  title: string;
+  route?: string;
+  children?: Menu[];
 }
