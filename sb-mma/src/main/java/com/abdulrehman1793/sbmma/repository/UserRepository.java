c25de6d9fb5408c76c390aa92c3703761b4ade35
Abdulrehman1793 <abdulrehman1793@gmail.com>
2023-10-04 19:55:31 +0530

    Add repositories

---
+++ sb-mma/src/main/java/com/abdulrehman1793/sbmma/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.abdulrehman1793.sbmma.repository;
+
+import com.abdulrehman1793.sbmma.model.auth.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByEmail(String email);
+}
